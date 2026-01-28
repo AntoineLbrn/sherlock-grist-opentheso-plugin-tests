9447fda0c5af766eafb8e55c0b9eb9d2b056d0b8
AntoineLbrn <antoinelmh@hotmail.fr>
2026-01-28 09:54:56 +0100

    trigger workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "src",
-    base: '/sherlock-grist-opentheso-plugin/',
+    base: '/sherlock-grist-opentheso-plugin-test/',
     build: {
         outDir: "../dist",
     },
